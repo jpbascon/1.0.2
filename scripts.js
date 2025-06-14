4385aa2c3a105ceaaa3229557d82b15892c162dc
jpbascon <jpbascon50@gmail.com>
2025-06-14 16:25:31 +0800

    Added email validation

--- scripts.js
+++ scripts.js
@@ -1,10 +1,14 @@
 const btnSubscribe = document.getElementById('js-btn-subscribe');
-const btnDismiss = document.querySelectorAll('.js-btn-dismiss');
-const formContainer = document.getElementById('form');
-const successFormDesktop = document.getElementById('desktop-form-success');
-const successFormMobile = document.getElementById('mobile-form-success');
+const formContainer = document.getElementById('js-form');
+const successFormDesktop = document.getElementById('js-desktop-form-success');
+const successFormMobile = document.getElementById('js-mobile-form-success');
 
-let active = false;
+const errorMessage = document.getElementById('js-error-message');
+const inputEmail = document.getElementById('js-email-validator');
+const btnDismiss = document.querySelectorAll('.js-btn-dismiss');
+const emailInput = document.querySelectorAll('.js-email');
+
+let active = true;
 let mobileDismiss = false;
 
 function updateMobileFlag() {
@@ -19,6 +23,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function btnSubscribeFtn() {
   btnSubscribe.addEventListener('click', () => {
+    const emailValue = inputEmail.value.trim();
+    if (!isValidEmail(emailValue)) {
+      errorMessage.style.display = 'block';
+      inputEmail.classList.add('active');
+      return;
+    } else {
+      errorMessage.style.display = 'none';
+      inputEmail.classList.remove('active');
+      active = false;
+    }
     if (!active) {
       formContainer.style.display = 'none';
       successFormDesktop.style.display = 'flex';
@@ -34,12 +48,18 @@ function btnSubscribeFtn() {
   })
 }
 
+inputEmail.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    btnSubscribe.click();
+  }
+})
+
 function btnDismissFtn() {
   btnDismiss.forEach(btn => {
     btn.addEventListener('click', () => {
       if (active) {
         if (mobile) {
-          formContainer.style.display = 'flex'
+          formContainer.style.display = 'none'
           if (mobileDismiss) {
             successFormMobile.style.display = 'none'
             mobileDismiss = false;
@@ -51,6 +71,15 @@ function btnDismissFtn() {
       }
     });
   });
+}
+
+function isValidEmail(email) {
+  // Simple email regex for demonstration
+  emailInput.forEach((e) => {
+    e.innerHTML = email;
+  })
+
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
 
 window.addEventListener('resize', updateMobileFlag);
